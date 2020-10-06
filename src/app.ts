564780d187d36823d41cc95297a4713f50b9411d
Daniel Cunha <danielcunha54@gmail.com>
2020-10-06 19:39:46 -0300

    feat: Create basic express application

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import 'dotenv/config'
+
+import express from 'express'
+import helmet from 'helmet'
+import cors from 'cors'
+import morgan from 'morgan'
+import 'express-async-errors'
+
+import routes from './routes'
+
+class App {
+  public express: express.Application
+
+  constructor() {
+    this.express = express()
+
+    this.middleware()
+    this.routes()
+  }
+
+  middleware() {
+    this.express.use(morgan('dev'))
+    this.express.use(helmet())
+    this.express.use(cors())
+    this.express.use(express.json())
+  }
+
+  routes() {
+    this.express.use(routes)
+  }
+}
+
+export default new App().express
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,6 @@
-console.log('hi there')
+import app from './app'
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Successfully started server on port \x1b[32m${port}\x1b[0m`)
+})
